de2a5d6a82360a51d3bab9c4ad6403870de304dd
Puppy124 <mrhuy12420011@gmail.com>
2023-12-27 15:06:32 +0700

    Added some more functions

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -9,18 +9,21 @@ private:
     int current_size;
 
 public:
+    // Default constructor if nothing is initialized from the vector
     MyVector() {
         arr = new T[1];
         capacity = 1;
         current_size = 0;
     }
 
+    // Constructor to initialize the elements from given values
     MyVector(std::initializer_list<T> values) : MyVector() {
         for (const auto& value : values) {
             add_back(value);
         }
     }
 
+    // Overload constructor to initialize values and amount of elements
     MyVector(int amount, int value) : current_size(amount), capacity(amount) {
         arr = new T[amount];
         for(int i = 0; i < amount; i++) {
@@ -28,52 +31,58 @@ public:
         }
     }
 
+    // Equivalent to push_back()
     void add_back(T data) {
         // If the vector size is equal to the capacity, double the capacity
         // to store new elements
         if (current_size == capacity) {
-            T* temp = new T[2 * capacity];
-
+            capacity *= 2;
+            T* new_array = new T[capacity];
             // Copy the content of old vector to new vector
             for (int i = 0; i < capacity; i++) {
-                temp[i] = arr[i];
+                new_array[i] = arr[i];
             }
-
-            delete[] arr;
-            capacity *= 2;
-            arr = temp;
+            delete[] arr;  // Delete the old vector to prevent memory leak
+            arr = new_array; // Assign the new array back to the original one
+            static int temp = 0;
+            std::cout << "Reallocated " << temp++ << " times." << std::endl;
         }
 
+        // Insert the new data into the vector then increase the current size by one
         arr[current_size] = data;
         current_size++;
     }
 
-    T get(int index) {
+    // Get the value at given index
+    T get_value(int index) {
         if (index < current_size) {
             return arr[index];
         }
     }
 
-    int size() {
-        return current_size;
-    }
+    int get_size() { return current_size; }
+
+    int get_capacity() { return capacity; }
 
     T* begin() { return arr; }
 
     T* end() { return arr + current_size; }
 
-    T operator[] (int index) {
-        return *(arr + index);
-    }
+    T operator[] (int index) { return *(arr + index); }
 
 };
 
 int main() {
     MyVector<int> d = {2, 5, 10};
-    MyVector<int> d1(10, 100);
 
-    for (const auto& elem : d1) {
+//    d.add_back(50);
+//    d.add_back(600);
+
+    for (const auto& elem : d) {
         std::cout << elem << std::endl;
     }
+    std::cout << d.get_size() << std::endl;
+    std::cout << d.get_capacity() << std::endl;
+
     return 0;
 }
